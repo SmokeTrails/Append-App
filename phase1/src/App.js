8de007de379345319095498a5f8aab11a2052590
Kirill Tregubov <imkirilltregubov@gmail.com>
2021-11-05 20:03:22 -0400

    Update App.js

--- phase1/src/App.js
+++ phase1/src/App.js
@@ -14,7 +14,7 @@ export default function App() {
 					<Route path="/" element={<Layout />}>
 						<Route index element={<Home />} />
 						<Route path="friends" element={<Friends />} />
-						<Route path="user/:id" element={<UserProfile />} />
+						<Route path="user/:username" element={<UserProfile />} />
 					</Route>
 					<Route path="*" element={<MissingPage />} />
 				</Routes>
