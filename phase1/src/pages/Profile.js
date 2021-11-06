2fb9c2e934861013de80b36a63b106fb1938d8d8
Haider <syed.bokhari@mail.utoronto.ca>
2021-11-06 19:59:22 -0400

    profile

--- phase1/src/pages/Profile.js
+++ phase1/src/pages/Profile.js
@@ -1,87 +1,7 @@
-import React, { useState, useEffect } from 'react';
+import React from 'react';
+import { useParams } from 'react-router';
+import PropTypes from "prop-types";
 import '../css/Main.css';
-
-// export default function Profile() {
-//     return(
-//         <div>
-//             <div id = "UserProfile">
-
-//                 <div className="ProfilePic">
-//                     <img style ={{width:"200px", height:"200px", borderRadius:"50%", border:"2px solid red"}}
-//                     src="https://st4.depositphotos.com/1156795/20814/v/950/depositphotos_208142514-stock-illustration-profile-placeholder-image-gray-silhouette.jpg" alt="Profile Picture" />
-//                 </div>
-//                 <div className = "ProfileDescription">
-//                     <h1>Haider Bokhari</h1>
-
-//                     <div className = "UserInfo">
-//                         <p style={{fontSize:"110%"}}>6 Friends&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;10 Clubs&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;5 Courses</p>
-//                     </div>
-
-//                     <p style={{fontSize:"130%", width: "1000px", height: "140px", marginBottom: "20px"}}><strong>Bio:</strong> Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate aute irure dolor in</p>
-//                     <p style={{fontSize:"120%", width: "1000px", height: "30px"}}><strong>Interests:</strong> #Lorem #ipsum #dolor</p>
-//                     <p style={{fontSize:"120%", width: "1000px", height: "30px"}}><strong>Current Year:</strong> 3&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;<strong>Program:</strong> Computer Science Specialist</p>
-
-//                 </div>
-//                 <div className = "ClubsCourses">
-//                     <h3>Current Courses</h3>
-//                         <ul className="gallery">
-//                             <li style={{background: "#f6bd60"}}><h3 style={{marginTop:"10px", marginLeft:"10px", fontWeight:"normal", wordWrap: "break-word"}}><strong>CSC309:</strong> Web Programming</h3>
-//                             <img style ={{width:"235px", height:"120px", border:"2px solid red", marginLeft: "5px", marginTop: "30px"}}
-//                             src="https://miro.medium.com/max/12000/0*tQQ7SLPOJfxaG4ZY" alt="Course Image" /></li>
-//                             <li style={{background: "#f7ede2"}}><h3 style={{marginTop:"10px", marginLeft:"10px", fontWeight:"normal", wordWrap: "break-word"}}><strong>CSC309:</strong> Web Programming</h3>
-//                             <img style ={{width:"235px", height:"120px", border:"2px solid red", marginLeft: "5px", marginTop: "30px"}}
-//                             src="https://miro.medium.com/max/12000/0*tQQ7SLPOJfxaG4ZY" alt="Course Image" /></li>
-//                             <li style={{background: "#f5cac3"}}><h3 style={{marginTop:"10px", marginLeft:"10px", fontWeight:"normal", wordWrap: "break-word"}}><strong>CSC309:</strong> Web Programming</h3>
-//                             <img style ={{width:"235px", height:"120px", border:"2px solid red", marginLeft: "5px", marginTop: "30px"}}
-//                             src="https://miro.medium.com/max/12000/0*tQQ7SLPOJfxaG4ZY" alt="Course Image" /></li>
-//                             <li style={{background: "#84a59d"}}><h3 style={{marginTop:"10px", marginLeft:"10px", fontWeight:"normal", wordWrap: "break-word"}}><strong>CSC309:</strong> Web Programming</h3>
-//                             <img style ={{width:"235px", height:"120px", border:"2px solid red", marginLeft: "5px", marginTop: "30px"}}
-//                             src="https://miro.medium.com/max/12000/0*tQQ7SLPOJfxaG4ZY" alt="Course Image" /></li>
-//                             <li style={{background: "#f28482"}}><h3 style={{marginTop:"10px", marginLeft:"10px", fontWeight:"normal", wordWrap: "break-word"}}><strong>CSC309:</strong> Web Programming</h3>
-//                             <img style ={{width:"235px", height:"120px", border:"2px solid red", marginLeft: "5px", marginTop: "30px"}}
-//                             src="https://miro.medium.com/max/12000/0*tQQ7SLPOJfxaG4ZY" alt="Course Image" /></li>
-//                         </ul>
-//                     <h3>Current Clubs</h3>
-//                         <ul className="gallery" style = {{gridTemplateColumns: "repeat(10, 250px)"}}>
-//                                 <li style={{background: "#f6bd60"}}><h3 style={{marginTop:"10px", marginLeft:"10px", fontWeight:"normal", wordWrap: "break-word"}}><strong>CSC309:</strong> Web Programming</h3>
-//                                 <img style ={{width:"235px", height:"120px", border:"2px solid red", marginLeft: "5px", marginTop: "30px"}}
-//                                 src="https://miro.medium.com/max/12000/0*tQQ7SLPOJfxaG4ZY" alt="Club Image" /></li>
-//                                 <li style={{background: "#f7ede2"}}><h3 style={{marginTop:"10px", marginLeft:"10px", fontWeight:"normal", wordWrap: "break-word"}}><strong>CSC309:</strong> Web Programming</h3>
-//                                 <img style ={{width:"235px", height:"120px", border:"2px solid red", marginLeft: "5px", marginTop: "30px"}}
-//                                 src="https://miro.medium.com/max/12000/0*tQQ7SLPOJfxaG4ZY" alt="Club Image" /></li>
-//                                 <li style={{background: "#f5cac3"}}><h3 style={{marginTop:"10px", marginLeft:"10px", fontWeight:"normal", wordWrap: "break-word"}}><strong>CSC309:</strong> Web Programming</h3>
-//                                 <img style ={{width:"235px", height:"120px", border:"2px solid red", marginLeft: "5px", marginTop: "30px"}}
-//                                 src="https://miro.medium.com/max/12000/0*tQQ7SLPOJfxaG4ZY" alt="Club Image" /></li>
-//                                 <li style={{background: "#84a59d"}}><h3 style={{marginTop:"10px", marginLeft:"10px", fontWeight:"normal", wordWrap: "break-word"}}><strong>CSC309:</strong> Web Programming</h3>
-//                                 <img style ={{width:"235px", height:"120px", border:"2px solid red", marginLeft: "5px", marginTop: "30px"}}
-//                                 src="https://miro.medium.com/max/12000/0*tQQ7SLPOJfxaG4ZY" alt="Club Image" /></li>
-//                                 <li style={{background: "#f28482"}}><h3 style={{marginTop:"10px", marginLeft:"10px", fontWeight:"normal", wordWrap: "break-word"}}><strong>CSC309:</strong> Web Programming</h3>
-//                                 <img style ={{width:"235px", height:"120px", border:"2px solid red", marginLeft: "5px", marginTop: "30px"}}
-//                                 src="https://miro.medium.com/max/12000/0*tQQ7SLPOJfxaG4ZY" alt="Club Image" /></li>
-//                                 <li style={{background: "#f6bd60"}}><h3 style={{marginTop:"10px", marginLeft:"10px", fontWeight:"normal", wordWrap: "break-word"}}><strong>CSC309:</strong> Web Programming</h3>
-//                                 <img style ={{width:"235px", height:"120px", border:"2px solid red", marginLeft: "5px", marginTop: "30px"}}
-//                                 src="https://miro.medium.com/max/12000/0*tQQ7SLPOJfxaG4ZY" alt="Club Image" /></li>
-//                                 <li style={{background: "#f7ede2"}}><h3 style={{marginTop:"10px", marginLeft:"10px", fontWeight:"normal", wordWrap: "break-word"}}><strong>CSC309:</strong> Web Programming</h3>
-//                                 <img style ={{width:"235px", height:"120px", border:"2px solid red", marginLeft: "5px", marginTop: "30px"}}
-//                                 src="https://miro.medium.com/max/12000/0*tQQ7SLPOJfxaG4ZY" alt="Club Image" /></li>
-//                                 <li style={{background: "#f5cac3"}}><h3 style={{marginTop:"10px", marginLeft:"10px", fontWeight:"normal", wordWrap: "break-word"}}><strong>CSC309:</strong> Web Programming</h3>
-//                                 <img style ={{width:"235px", height:"120px", border:"2px solid red", marginLeft: "5px", marginTop: "30px"}}
-//                                 src="https://miro.medium.com/max/12000/0*tQQ7SLPOJfxaG4ZY" alt="Club Image" /></li>
-//                                 <li style={{background: "#84a59d"}}><h3 style={{marginTop:"10px", marginLeft:"10px", fontWeight:"normal", wordWrap: "break-word"}}><strong>CSC309:</strong> Web Programming</h3>
-//                                 <img style ={{width:"235px", height:"120px", border:"2px solid red", marginLeft: "5px", marginTop: "30px"}}
-//                                 src="https://miro.medium.com/max/12000/0*tQQ7SLPOJfxaG4ZY" alt="Club Image" /></li>
-//                                 <li style={{background: "#f28482"}}><h3 style={{marginTop:"10px", marginLeft:"10px", fontWeight:"normal", wordWrap: "break-word"}}><strong>CSC309:</strong> Web Programming</h3>
-//                                 <img style ={{width:"235px", height:"120px", border:"2px solid red", marginLeft: "5px", marginTop: "30px"}}
-//                                 src="https://miro.medium.com/max/12000/0*tQQ7SLPOJfxaG4ZY" alt="Club Image" /></li>
-
-//                             </ul>
-
-//                 </div>
-
-//             </div>
-//         </div>
-//     )
-// }
 
 function CardItem(props) {
 	return (
@@ -110,13 +30,44 @@ function ProfileDescription(props) {
 			<div className="UserInfo">
 				<p style={{ fontSize: "110%" }}>{props.friendCount} Friends&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;{props.clubCount} Clubs&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;{props.courseCount} Courses</p>
 			</div>
-
-			<p style={{ fontSize: "130%", width: "1000px", height: "140px", marginBottom: "20px" }}><strong>Bio:</strong>{props.bio}</p>
-			<p style={{ fontSize: "120%", width: "1000px", height: "30px" }}><strong>Interests:</strong> {props.interests}</p>
-			<p style={{ fontSize: "120%", width: "1000px", height: "30px" }}><strong>Current Year:</strong> {props.year}&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;<strong>Program:</strong> {props.program}</p>
-
 		</div>
 	);
+}
+
+function ProfileInfo(props){
+	return(
+		<div>
+			<p style={{ fontSize: "120%"}}><strong>Bio:</strong></p>
+			<p style={{ fontSize: "130%", width: "1000px", height: "140px", marginBottom: "20px" }}>{props.bio}</p>
+			<p style={{ fontSize: "120%", width: "1000px", height: "30px" }}><strong>Interests:</strong> {props.interests}</p>
+			<p style={{ fontSize: "120%", width: "1000px", height: "30px" }}><strong>Current Year:</strong> {props.year}&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;<strong>Program:</strong> {props.program}</p>
+		</div>
+	)
+}
+
+function ProfileEditingInfo(props){
+	return(
+		<form>
+			<label>
+				<p style={{ fontSize: "120%"}}><strong>Bio:</strong></p>
+				<input defaultValue = {props.bio} type="textarea" name="bio" style={{ fontSize: "130%", width: "1000px", height: "140px", marginBottom: "20px" }} maxLength="500"/>
+			</label>
+			<label>
+				<p style={{ fontSize: "120%", width: "1000px", height: "30px" }}><strong>Interests:</strong></p>
+				<input defaultValue = {props.interests} type="text" name="interests" style={{ fontSize: "120%", width: "1000px", height: "30px" }}/>
+			</label>
+			<label>
+			<p style={{ fontSize: "120%", width: "1000px", height: "30px" }}><strong>Current Year:</strong></p>
+				<input defaultValue = {props.year} type="number" name="year" style={{ fontSize: "120%", width: "1000px", height: "30px" }}/>
+			</label>
+			<label>
+			<p style={{ fontSize: "120%", width: "1000px", height: "30px" }}><strong>Program:</strong></p>
+				<input defaultValue = {props.program} type="text" name="program" style={{ fontSize: "120%", width: "1000px", height: "30px" }}/>
+			</label>
+
+			<input type="submit" value="Save" />
+		</form>
+	)
 }
 
 function GalleryView(props) {
@@ -133,12 +84,20 @@ function GalleryView(props) {
 	);
 }
 
-export default class UserProfile extends React.Component {
+// function UsernameHook(Component) {
+// 	return function WrappedComponent(props) {
+// 		const username = useParams();
+// 	  return <UserProfile {...props} username={username} />;
+// 	}
+//   }
+
+class UserProfile extends React.Component {
 	constructor(props) {
 		super(props);
 		this.editClick = this.editClick.bind(this);
 		this.saveClick = this.saveClick.bind(this);
-		this.state = { isEditing: false }
+		this.state = { isEditing: false };
+		this.username = props.username
 	}
 	
 	editClick() {
@@ -149,6 +108,7 @@ export default class UserProfile extends React.Component {
 		this.setState({ isEditing: false })
 	}
 
+	
 	render() {
 		const isEditing = this.state.isEditing;
 		let button;
@@ -164,28 +124,21 @@ export default class UserProfile extends React.Component {
 
 		return (
 			<div>
+				{this.username === 'Haider' &&
+					{button}
+				}
 				<Avatar />
-				<ProfileDescription name='Haider Bokhari' friendCount='3' clubCount='10' courseCount='5' bio='Hello 123' interests='#1 #test #2' year='3' program='Computer Science Specialist' />
-				<GalleryView name='Current Courses' />
-				<GalleryView name='Current Clubs' />
+				<ProfileDescription name='Haider' friendCount='3' clubCount='10' courseCount='5'/>
+				{isEditing &&
+					<ProfileEditingInfo bio='Hello 123' interests='#1 #test #2' year='3' program='Computer Science Specialist'/>
+				}
+				{!isEditing &&
+					<ProfileInfo bio='Hello 123' interests='#1 #test #2' year='3' program='Computer Science Specialist'/>
+				}
+				
+				<GalleryView title='Current Courses' />
+				<GalleryView title='Current Clubs' />
 			</div>
 		)
 	}
 }
-
-
-// function editButton(){
-//     console.log(up);
-//     const ProfileDesc = up.children[2];
-//     const bio = ProfileDesc.children[2];
-//     const button = ProfileDesc.children[0];
-//     const input = document.createElement('input');
-//     input.type = 'text';
-//     input.style.width = '1000px';
-//     input.style.width = '140px';
-//     input.style.fontSize = '130%';
-//     input.value = bio.textContent;
-//     ProfileDesc.insertBefore(input, bio);
-//     ProfileDesc.removeChild(bio);
-//     //this.changeText = 'Save';
-// }
