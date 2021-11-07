a671ec42f0da93e7ddca8c464956f6fd4b95c255
Haider <syed.bokhari@mail.utoronto.ca>
2021-11-07 02:52:36 -0500

    Save Edit Profile

--- phase1/src/pages/Profile.js
+++ phase1/src/pages/Profile.js
@@ -3,9 +3,13 @@ import { useParams } from 'react-router';
 // import PropTypes from "prop-types";
 import '../css/Main.css';
 
+const generateKey = (pre) => {
+    return `${ pre }_${ new Date().getTime() }`;
+}
+
 function CardItem(props) {
 	return (
-		<li style={{ background: "#f6bd60" }}><h3 style={{ marginTop: "10px", marginLeft: "-50px", fontWeight: "normal", wordWrap: "break-word" }}><strong>{props.courseCode}:</strong> {props.courseTitle}</h3>
+		<li key={generateKey(props.courseCode)} style={{ background: "#f6bd60" }}><h3 style={{ marginTop: "10px", marginLeft: "-50px", fontWeight: "normal", wordWrap: "break-word" }}><strong>{props.courseCode}:</strong> {props.courseTitle}</h3>
 			<img style={{ width: "235px", height: "120px", border: "2px solid red", marginLeft: "5px", marginTop: "30px" }}
 				src="https://miro.medium.com/max/12000/0*tQQ7SLPOJfxaG4ZY" alt="Group Banner" /></li>
 	);
@@ -46,8 +50,23 @@ function ProfileInfo(props) {
 }
 
 function ProfileEditingInfo(props) {
+	const handleSubmit= (e) => {
+		e.preventDefault();
+
+		var Users = users.filter(user => {
+			return user.username === props.username
+		})
+
+		Users[0].bio = e.target[0].value
+		Users[0].interests = e.target[1].value
+		Users[0].year = e.target[2].value
+		Users[0].program = e.target[3].value
+
+		props.setIsEditing(false);
+	  }
+
 	return (
-		<form>
+		<form onSubmit = {e => {handleSubmit(e)}}>
 			<label>
 				<p style={{ fontSize: "120%" }}><strong>Bio:</strong></p>
 				<input defaultValue={props.bio} type="textarea" name="bio" style={{ fontSize: "130%", width: "1000px", height: "140px", marginBottom: "20px" }} maxLength="500" />
@@ -65,21 +84,21 @@ function ProfileEditingInfo(props) {
 				<input defaultValue={props.program} type="text" name="program" style={{ fontSize: "120%", width: "1000px", height: "30px" }} />
 			</label>
 
-			<input type="submit" value="Save" />
+			<input type="submit" value="Save"/>
 		</form>
 	)
 }
 
+
 function GalleryView(props) {
 	var cards = props.items
-	var count = 0
 	const AllCards = cards.map((item) =>
-  		<li><CardItem courseCode={item} courseTitle="Lorem Ipsum" /></li>
+  		<CardItem courseCode={item} courseTitle="Lorem Ipsum" />
 	);
 
 	return(
 		<div>
-			<h3 style={{justifyContent:'left'}}>{props.title}</h3>
+			<h2>{props.title}</h2>
 			<ul className = "gallery" style={{gridTemplateColumns: `repeat(${AllCards.length}, 250px)`}}>{AllCards}</ul>
 		</div>
 	);
@@ -179,14 +198,14 @@ export default function UserProfile() {
 	return (
 		<div>
 			{ username === 'user' &&
-				<button onClick={isEditing ? saveClick : editClick}>{isEditing ? 'Save' : 'Edit'}</button>
+				<button onClick={isEditing ? saveClick : editClick}>{isEditing ? '' : 'Edit'}</button>
 			}
 
 			<Avatar imageURL={("imageUrl" in User) ? User.imageUrl : 'DefaultPic.jpg'}/>
 			
 			<ProfileDescription name={User.name} username={User.username} friendCount={User.friendCount} clubCount={User.clubCount} courseCount={User.courseCount} />
 			{isEditing &&
-				<ProfileEditingInfo bio={User.bio} interests={User.interests} year={User.year} program={User.program} />
+				<ProfileEditingInfo setIsEditing={setIsEditing} username={User.username} bio={User.bio} interests={User.interests} year={User.year} program={User.program} />
 			}
 			{!isEditing &&
 				<ProfileInfo bio={User.bio} interests={User.interests} year={User.year} program={User.program} />
