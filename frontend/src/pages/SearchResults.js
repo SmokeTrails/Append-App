b9998dd3653b47375c55726450bebccadc5fd4b7
mohsinreza <mohsin.reza@mail.utoronto.ca>
2021-12-07 18:57:27 -0500

    Synced search results with backend

--- frontend/src/pages/SearchResults.js
+++ frontend/src/pages/SearchResults.js
@@ -1,82 +1,74 @@
-import React, { useState } from 'react';
+import React, { useState, useEffect } from 'react';
 import { useParams } from "react-router-dom";
 import Search from '../components/Search';
 import FriendPreview from '../components/FriendPreview';
 import CommunityPreview from '../components/CommunityPreview';
+import env from '../config.js'
+const api_host = env.api_host
 
 export default function SearchResults() {
 	// Users need to be fetched from backend
-	const [users] = useState([
-		{
-			name: 'Haider',
-			username: 'user'
-		},
-		{
-			name: 'Alex D',
-			username: 'AlexDobbin'
-		},
-		{
-			name: 'Joshua Lee',
-			username: 'Marvin'
-		},
-		{
-			name: 'Kirill',
-			username: 'KirillTregubov',
-			imageUrl: 'users/kirill.png'
-		},
-		{
-			name: 'Mohsin',
-			username: 'SmokeTrails'
-		},
-		{
-			name: 'Rehan',
-			username: 'TheRayman786'
-		}
-	])
+	const query = useParams().query;
+	const [isLoading, setIsLoading] = useState(true)
+	const [users, setUsers] = useState([])
 
 	// Communities need to be fetched from backend
-	const [communities] = useState([
-		{
-			path: 'csc309',
-			name: 'CSC309',
-			description: "This course provides an introduction to the technologies used for developing Web applications. We discuss technologies for static and dynamic content generation, including N-tier, MVC architectures, and mobile supported web development. We also cover general web design principles, security, and web performance.",
-			memberCount: 1012,
-			imageUrl: 'communities/csc309.jpg'
-		},
-		{
-			path: 'csc301',
-			name: 'CSC301',
-			description: "An introduction to agile development methods appropriate for medium-sized teams and rapidly-moving projects. Basic software development infrastructure; requirements elicitation and tracking; estimation and prioritization; teamwork skills; basic UML; design patterns and refactoring; security, discussion of ethical issues, and professional responsibility.",
-			memberCount: 536,
-			imageUrl: 'communities/csc301.jpg'
-		},
-		{
-			path: 'AnimeClub',
-			name: 'Anime Club',
-			description: "U of T's largest anime club. We have weekly meetings but feel free to make a post on the forum.",
-			memberCount: 1998,
-			imageUrl: 'communities/anime.jpg'
-		},
-		{
-			path: 'WebDevClub',
-			name: 'Web dev Club',
-			description: "U of T's largest anime club. We have weekly meetings but feel free to make a post on the forum.",
-			memberCount: 405,
-			imageUrl: 'communities/webdev.jpg'
-		}
-	])
+	const [communities, setCommunities] = useState([])
 
-	const query = useParams().query;
-	const matchingCommunities = communities.filter((community) => community.name.toLowerCase().includes(query.toLowerCase()))
-	const matchingUsers = users.filter((user) => user.username.toLowerCase().includes(query.toLowerCase()) || user.name.toLowerCase().includes(query.toLowerCase()))
+	useEffect(() => {
+		if (isLoading === true) {
+			fetch(`${api_host}/users`)
+			.then(res => {
+				// console.log("The status is", res.status)
+				if (res.status === 200) {
+					return res.json();
+				}
+			})
+			.then(json => {
+				console.log("Users found: ", json)
+				if (json) {
+					// console.log('setting user')
+					setUsers(json);
+					console.log("Users before filter: ", users)
+					setUsers(users.filter((user) => user.username.toLowerCase().includes(query.toLowerCase()) || user.name.toLowerCase().includes(query.toLowerCase())))
+				}
+			})
+			.catch(error => {
+				console.log(error);
+			});
+			fetch(`${api_host}/community`)
+			.then(res => {
+				// console.log("The status is", res.status)
+				if (res.status === 200) {
+					return res.json();
+				}
+			})
+			.then(json => {
+				console.log("Communities found: ", json)
+				if (json) {
+					// console.log('setting user')
+					setCommunities(json);
+					console.log("Communities before filter: ", communities)
+					setCommunities(communities.filter((community) => community.name.toLowerCase().includes(query.toLowerCase())))
+					setIsLoading(false)
+				}
+			})
+			.catch(error => {
+				console.log(error);
+			});
+		}
+	}, [isLoading]);
 
 	return (
 		<div>
-			<Search query={query} />
-			<h1 className="heading"> Users </h1>
-			<MatchingProfiles users={matchingUsers} />
-			<h1 className="heading"> Communities </h1>
-			<MatchingCommunities communities={matchingCommunities} />
+			{
+				isLoading ? 'Loading...'
+				: <div> <Search query={query} />
+				<h1 className="heading"> Users </h1>
+				<MatchingProfiles users={users} />
+				<h1 className="heading"> Communities </h1>
+				<MatchingCommunities communities={communities} /> </div>
+			}
 		</div>
 	)
 }
@@ -89,7 +81,7 @@ function MatchingProfiles(props) {
 			<div>
 				{props.users && props.users.map((user, index) =>
 					<div key={index}>
-						<FriendPreview name={user.name} username={user.username} imageUrl={user.imageUrl} />
+						<FriendPreview simple={false} name={user.name} username={user.username} imageUrl={user.imageUrl} />
 					</div>
 				)}
 			</div>
@@ -104,7 +96,7 @@ function MatchingCommunities(props) {
 		return (
 			<div>
 				{props.communities && props.communities.map((community, index) =>
-					<CommunityPreview key={index} path={community.path} name={community.name} description={community.description} memberCount={community.memberCount} imageUrl={community.imageUrl} />
+					<CommunityPreview key={index} path={community.path} name={community.name} description={community.description} memberCount={community.members.length} imageUrl={community.imageUrl} />
 				)}
 			</div>
 		)
