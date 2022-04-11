409a12403ec9888e6e35ab0ceb6fec675514b8aa
SmokeTrails <76517921+SmokeTrails@users.noreply.github.com>
2022-04-11 15:33:22 -0400

    Update mongoose.js

--- backend/mongoose.js
+++ backend/mongoose.js
@@ -1,10 +1,9 @@
     
     const mongoose = require('mongoose')
 
-	const DB = 'mongodb+srv://team51:54321@cluster0.dsirf.mongodb.net/Team51?retryWrites=true&w=majority';
+	const DB = 'REMOVED CONNECTION STRING';
     /* Connnect to our database */
     // Get the URI of the local database, or the one specified on deployment.
-    //DB = process.env.MONGODB_URI || "mongodb+srv://team51:54321@cluster0.dsirf.mongodb.net/team51?retryWrites=true&w=majority"
     
 	//{ useNewUrlParser: true, useUnifiedTopology: true, useCreateIndex: true}
     mongoose.connect(DB)
